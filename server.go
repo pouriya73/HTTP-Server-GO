68f9323a1e0d7c73f49b3d07908d5f54de40d7c7
Pourya <cepsociety@gmail.com>
2022-04-18 16:47:50 +0300

    Update server.go

--- server.go
+++ server.go
@@ -7,6 +7,10 @@ import (
     "net/http"
 )
 
+//A fundamental concept in net/http servers is handlers.
+//A handler is an object implementing the http.Handler interface. 
+//A common way to write a handler is by using the http.
+//HandlerFunc adapter on functions with the appropriate signature.
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
